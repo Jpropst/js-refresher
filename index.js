bbec0663e62d50ddf50d224ad8f2eb1394395cd2
Joshua <Jpropst35@gmail.com>
2023-07-24 18:08:24 -0400

    js refresher

---
+++ index.js
@@ -0,0 +1,25 @@
+const firstName = "Joshua";
+const lastName = "Propst";
+const fullName = firstName.concat(" " + lastName);
+console.log(fullName);
+
+let population = 1625000;
+console.log(population);
+let total = 1625000 * 3;
+console.log(total)
+
+const javaScriptIsCool = true;
+
+const color1 = "red";
+const color2 = "orange";
+const color3 = "yellow";
+const color4 = "green";
+const color5 = "blue";
+const color6 = "indigo";
+const color7 = "violet";
+const colors = color1.concat(" ", color2, " ", color3, " ", color4, " ", color5, " ", color6, " ", color7);
+
+for (let num = 1; num < 6; num++)
+console.log(num);
+for (let num = 0; num <5; num++)
+console.log(num);
